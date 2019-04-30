811546f2ea669af3bcd0bf9428a92300718dd3f0
Daniel Marschner <marschner.leinad@gmail.com>
2019-04-30 20:53:55 +0200

    Added light and dark theme;

---
+++ marschner.io.js
@@ -0,0 +1,23 @@
+var $ = document;
+var today = new Date();
+var time = today.getHours();
+var id = 'dark';
+
+if (time >= 6 && time <= 19) {
+  id = "light";
+}
+
+id = "light";
+
+if (!$.getElementById(id)) {
+  var head = $.getElementsByTagName('head')[0];
+  var link = $.createElement('link');
+
+  link.id = id;
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = id + '.css';
+  link.media = 'all';
+
+  head.appendChild(link);
+}
